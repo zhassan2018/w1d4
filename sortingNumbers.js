39a4c01881028dfa6e620eee3307d0897b6c9f0b
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-07 15:31:12 +0000

    Sorting Object

--- sortingNumbers.js
+++ sortingNumbers.js
@@ -1,5 +1,23 @@
-var array = [10, 2, 5, 1, 9];
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
 
-console.log(array.sort(function(a,b){
-	return (a-b)
-}))
+
+var check = students.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return a.age - b.age;
+});
+
+console.log(check)
