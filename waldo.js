d9c4709cb4931c14d69d76f1217d4e557fd9e42b
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-07 01:03:27 +0000

    Print the index of Walso when found

---
+++ waldo.js
@@ -0,0 +1,15 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(i) {
+	
+  console.log("Found waldo at index " + i + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
